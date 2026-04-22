ac146ee703b85149e9ad22dd27ec46e2147aea1a
Xyiee <rakahabibi@student.plai.ac.id>
2026-04-22 11:21:10 +0700

    C++_github

--- main.cpp
+++ main.cpp
@@ -20,3 +20,18 @@ bool isSecure(string pwd) {
     
     return hasNum && isLongEnough;
 }
+
+#raka
+int main() {
+    displayHeader();
+    string password = "Abcde!2345";
+    string cipherText = encryptData(password);
+    
+    cout << "Encrypted password is " << cipherText << endl;
+    
+    if (isSecure(password)) {
+        cout << "Password is secure!" << endl;
+    } else {
+        cout << "Password is not secure!" << endl;
+    }
+}
